95c0e138b944da19d5d169f4c4e05ca44dc4e1dd
Iagodizlla <iagoschlemper@gmail.com>
2025-03-12 15:44:11 -0300

    Adicionar arquivos de projeto.

---
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace JogoDaForca.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("_____________");
+            Console.WriteLine("Jogo da forca");
+            Console.WriteLine("-------------\n");
+
+            string palavraSecreta = "Among-Us";
+            string palavraDescoberta = "_";
+            Console.WriteLine("A palavra secreta é: ");
+            for (int i = 0; i < palavraSecreta.Length; i++)
+            {
+                Console.Write(palavraDescoberta);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
